9f079df4d6edf585959e575afa0f9bb10a33afdc
GittingItOnWithYourMom <146044625+GittingItOnWithYourMom@users.noreply.github.com>
2025-11-09 18:58:51 -0800

    voicelines
    1 hour remaining duh duh

--- Mini-Jam/Scenes/Scripts/sfx.gd
+++ Mini-Jam/Scenes/Scripts/sfx.gd
@@ -2,8 +2,16 @@ extends Node
 
 @export var cracking = true
 @export var ambience = true
+@export var Mr_Johnson = true
+@onready var beep = $intercom/beep
+@onready var intercom = $intercom/intercom
+@onready var intro = $intercom/intro
+
 
 func _ready():
+	if Mr_Johnson == false:
+		var index = AudioServer.get_bus_index("Mr Johnson")
+		AudioServer.set_bus_mute(index, true)
 	if cracking == false:
 		$crack1.volume_db = -80
 		$crack2.volume_db = -80
@@ -11,3 +19,21 @@ func _ready():
 		$crack4.volume_db = -80
 	if ambience == false:
 		$ambience.volume_db = -80
+	await get_tree().create_timer(7).timeout
+	beep.play()
+	await beep.finished
+	intro.play()
+	await intro.finished
+	beep.play()
+	$intercom/intercom_timer.start()
+
+func random_intercom():
+	beep.play()
+	await beep.finished
+	intercom.play()
+	await intercom.finished
+	beep.play()
+
+
+func _on_intercom_timer_timeout():
+	random_intercom()
